c32581a02253465d1f096c23d94fb49a064fb48f
Kr2-004 <cansecocarlos008@gmail.com>
2025-10-31 02:02:07 +0000

    Corrected launch files

--- launch/puzzlebot2.launch.py
+++ launch/puzzlebot2.launch.py
@@ -5,8 +5,8 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='dmpc_controller',
-            executable='dmpc_node',
-            name='dmpc_node',
+            executable='dmpc_cbf_node',
+            name='dmpc_cbf_node',
             output='screen',
             parameters=[{'robot_name': 'puzzlebot2'}]
         )
